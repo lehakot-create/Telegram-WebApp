fd250c0cb919a320785a014a0214543fde11e282
lehakot-create <lehakot85@inbox.ru>
2023-01-15 23:12:17 +0400

    перенес script в body

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const btn = document.querySelector('button')
 
 btn.onclick = ()=>{
-    alert(`Hello${WebApp.WebAppUser.username}`)
+    alert(`Hello${WebApp.WebAppUser.initData}`)
 }
 
 const WebApp = window.Telegram.WebApp;
 
 WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
+
+// const initData = window.Telegram.WebApp.initData
+
