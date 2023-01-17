0694f8254336712ce500c45f0f9852e019f1fd8a
lehakot-create <lehakot85@inbox.ru>
2023-01-17 19:44:44 +0400

    a

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const btn = document.querySelector('button')
 
 btn.onclick = ()=>{
+    let WebApp = window.Telegram.WebApp;
+    WebApp.showAlert(`Добро пожаловать, @${WebApp.WebAppUser.username}.`);
+
     alert(`Hello from telegram`)
     const initData = window.Telegram.WebApp.initData
     console.log(initData)
